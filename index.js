1b26024ead9601e44d6928c8614d70729879a727
ppinto-pplware <ppinto@McBkPro-Pplware.local>
2020-12-11 18:05:41 +0000

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+//Get the framework for hapi.js
+const Hapi = require('hapi');
+
+// Store the hosting server and the port 
+const host = 'localhost';
+const port = 3000; 
+
+// Lets create the server here
+const server = Hapi.Server({
+    host: host,
+    port: port
+});
+
+// Create an init method to start the server. 
+const init = async () => {
+
+    await server.start();
+    console.log("Server up and running at port: " + port);
+
+}
+
+//Definir routes
+require('./routes/routes')(server);
+
+// Call the init method.
+init();
+
